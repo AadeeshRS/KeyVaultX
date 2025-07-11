0ea45273ac231e279ad30bfc5f0320f8d48ebf0a
Aadeesh <srivastavaaadeesh@gmail.com>
2025-07-11 19:01:15 +0530

    Changed the footer to be more responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full z-50 bg-slate-900 backdrop-blur-sm border-b border-white/10 text-white flex p-4 justify-center items-center h-10 fixed bottom-0'>
-       <div className="logo font-bold text-xl text-white">
-                <span className='text-yellow-500'>
-                &lt;
-                </span>
-                KeyVault<span className='text-yellow-500'>X/&gt;</span>
-                </div>
-                <div className='text-center mx-1'>© 2025 KeyVaultX. All rights reserved.</div>
-
+    <div className='w-full z-50 bg-slate-900 backdrop-blur-sm border-t border-white/10 text-white flex flex-col sm:flex-row p-2 sm:p-3 justify-center items-center h-auto sm:h-10 fixed bottom-0'>
+      <div className="flex items-center">
+        <div className="logo font-bold text-lg sm:text-xl text-white whitespace-nowrap">
+          <span className='text-yellow-500'>&lt;</span>
+          KeyVault<span className='text-yellow-500'>X/&gt;</span>
+        </div>
+        <div className='text-xs sm:text-sm mx-1 sm:mx-2 text-center sm:text-left'>
+          © 2025 KeyVaultX. All rights reserved.
+        </div>
+      </div>
     </div>
   )
 }
